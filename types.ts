0e53c401fa4fd19a9eb04d74bc0b39c9e64887e9
SebasUrbina <sebastian.urbina@live.cl>
2025-02-21 17:44:06 -0300

    ✨ Add home card and transaction list components
    - Created HomeCard component with balance, income, and expense overview
    - Implemented TransactionList component with dynamic transaction rendering
    - Added new types for transactions, categories, and related props
    - Integrated Phosphor icons and Animated components for enhanced UI

--- types.ts
+++ types.ts
@@ -1,4 +1,6 @@
+import { Icon } from "phosphor-react-native";
 import { ViewStyle } from "react-native";
+import { Timestamp } from "firebase/firestore";
 
 export type UserDataType = {
   name: string;
@@ -29,4 +31,40 @@ export type ImageUploadProps = {
   containerStyle?: ViewStyle;
   imageStyle?: ViewStyle;
   placeholder?: string;
+};
+
+export type TransactionType = {
+  id?: string;
+  type: string;
+  amount: number;
+  category?: string;
+  date: Date | Timestamp | string;
+  description?: string;
+  image?: any;
+  uid?: string;
+  walletId: string;
+};
+
+export type TransactionListType = {
+  data: TransactionType[];
+  title?: string;
+  loading?: boolean;
+  emptyListMessage?: string;
+};
+
+export type TransactionItemProps = {
+  item: TransactionType;
+  index: number;
+  handleClick: Function;
+};
+
+export type CategoryType = {
+  label: string;
+  value: string;
+  icon: Icon;
+  bgColor: string;
+};
+
+export type ExpenseCategoriesType = {
+  [key: string]: CategoryType;
 };
